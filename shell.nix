a17a2411fe2200fc6e2213ca0ab6af34ef52d5c6
TruncatedDinosour <71613062+TruncatedDinosour@users.noreply.github.com>
2021-12-28 15:16:10 +0000

    Update NixOS dependencies

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> {};
 stdenv.mkDerivation rec {
   name = "yafetch-env";
   buildInputs = with pkgs; [
-    lua51Packages.lua
+    lua54Packages.lua
   ];
   nativeBuildInputs = with pkgs; [ pkg-config ];
 }
