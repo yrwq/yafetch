87608a0f6635d737c49ad0b331187833c7e6ba23
Inhof Dávid <yrwqid@gmail.com>
2022-03-15 20:54:25 +0100

    experimenting with rlua

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use rlua::Lua;
+
+fn main()  {
+    let lua = Lua::new();
+
+    lua.context(|ctx| {
+        let globals = ctx.globals();
+
+        globals.set("string_var", "hello").unwrap();
+        globals.set("int_var", 42).unwrap();
+        let i: i32 = globals.get("int_var").unwrap();
+        println!("{}", i)
+    });
 }
