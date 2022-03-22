a1f3e5224f4c082ccd4f555381b8503fae850c4f
Inhof Dávid <yrwqid@gmail.com>
2022-03-22 08:42:51 +0100

    simplify getting path of the configuration dir

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,14 +1,8 @@
 pub fn get_config() -> String {
-    let mut config_location: String;
+    let mut home_cfg: String = std::env::var("HOME").unwrap();
+    home_cfg.push_str("/.config");
 
-    let name = "XDG_CONFIG_HOME";
-    let mut home: String = std::env::var("HOME").unwrap();
-    home.push_str("/.config");
-
-    match std::env::var(name) {
-        Ok(v) => config_location = v,
-        Err(_e) => config_location = home
-    }
+    let mut config_location = std::env::var("XDG_CONFIG_HOME").unwrap_or(home_cfg);
 
     config_location.push_str("/yafetch/init.lua");
 
